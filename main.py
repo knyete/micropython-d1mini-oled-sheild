a006339767db11205728d2dc0d53187d6b04b6a2
Reilly <reilly@freecyber.space>
2018-07-22 17:06:07 +1000

    GPS module

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import network
 import wifi_config
 from umqtt_simple import MQTTClient
 import mqtt_config
-from sht30 import SHT30
-from machine import Pin
+from machine import Pin, UART
 from time import sleep
+from micropyGPS import MicropyGPS
 
 wifi = network.WLAN(network.STA_IF)
 
@@ -14,18 +14,30 @@ if not wifi.isconnected():
 	while not wifi.isconnected():
 		sleep(1)
 
-c = MQTTClient(mqtt_config.CLIENT_ID, mqtt_config.SERVER)
-c.set_last_will(b"sensors/status/" + mqtt_config.CLIENT_ID, "OFFLINE")
-c.connect()
-c.publish(b"sensors/status/" + mqtt_config.CLIENT_ID, b"ONLINE")
+mqtt = MQTTClient(mqtt_config.CLIENT_ID, mqtt_config.SERVER)
+# mqtt.set_last_will(b"sensors/status/" + mqtt_config.CLIENT_ID, "OFFLINE")
+mqtt.connect()
+mqtt.publish(b"sensors/status/" + mqtt_config.CLIENT_ID, b"ONLINE")
 
-sensor = SHT30()
-blue_led = Pin(2, Pin.OUT)
+uart = UART(2, 9600)
+uart.init(9600, bits=8, parity=None, stop=1)
+
+gps = MicropyGPS(location_formatting = "dd")
 
 while True:
-        blue_led.off()
-        temperature, humidity = sensor.measure()
-	c.publish(b"sensors/temperature/" + mqtt_config.CLIENT_ID, str(temperature))
-	c.publish(b"sensors/humidity/" + mqtt_config.CLIENT_ID, str(humidity))
-        blue_led.on()
-	sleep(10 * 60)
+        data = uart.read()
+        if data != None:
+                for c in str(data):
+                        gps.update(c)
+
+                # 1 = no fix, 2 = 2D fix, 3 = 3D fix                        
+                if gps.fix_type != 1 and gps.satellite_data_updated() and gps.time_since_fix() < 1000:
+                        lat, ns = gps.latitude
+                        lat *= 1 if ns == 'N' else -1
+                        lon, ew = gps.longitude
+                        lon *= 1 if ew == 'E' else -1
+                        s = "%.6f,%.6f,%.1f,%d" % (lat, lon, gps.pdop, gps.satellites_in_use)
+                        print(s)
+                        mqtt.publish(b"sensors/gps/" + mqtt_config.CLIENT_ID, s)
+                        # Dilution of Precision (DOP) values close to 1.0 indicate excellent quality position data
+                        sleep(int(120 / gps.hdop))
